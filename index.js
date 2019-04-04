2c0df37b24afdb92db9dfc1f763844f0c4dafac0
szydan <danielczyk.szymon@gmail.com>
2019-04-04 12:09:22 +0100

    backport preInit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function (kibana) {
   return new kibana.Plugin({
     name: 'kibi_radar_vis',
-    require: ['kibana', 'elasticsearch'],
+    require: ['kibana', 'elasticsearch', 'migrations'],
     uiExports: {
       visTypes: [
         'plugins/kibi_radar_vis/kibi_radar_vis'
